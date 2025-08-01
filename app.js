9fd018df001f026ad9f95306bd5c25ed8ce7f035
MariaBap <128877946+MariaBap@users.noreply.github.com>
2025-07-31 22:29:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require('http');
+const os = require('os');
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello! This is a Node.js app running in Kubernetes\n');
+  res.end(`Hello! This is a Node.js app running in Kubernetes\nHostname: ${os.hostname()}\n`);
 });
 
 server.listen(PORT, () => {
